ed7bfe8619967913b87ab8d1d63243d954490b0f
unknown <unknown@example.com>
2018-10-21 21:22:48 +0800

    test
    test

--- pages/list/list.js
+++ pages/list/list.js
@@ -4,5 +4,15 @@ const app = getApp()
 
 Page({
   onLoad: function () {
+  },
+  onclick: function(){
+    console.log('test')
+    wx.navigateTo({
+      url: '../add_content/add_content'
+    })
   }
 })
+
+function onclick() {
+  console.log('test')  
+}
