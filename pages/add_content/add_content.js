a934ef42bd871915a481d5acf22204023527b396
unknown <unknown@example.com>
2018-10-23 00:51:25 +0800

    add logs view

--- pages/add_content/add_content.js
+++ pages/add_content/add_content.js
@@ -3,7 +3,44 @@
 const app = getApp()
 
 Page({
+  data : {
+    note_title: "",
+    note_type: "",
+    note_content: "" 
+  },
   onLoad: function () {
+
+  },
+  note_title: function(e){
+    this.data.note_title = e.detail.value;
+  },
+  note_type: function(e){
+    this.data.note_type = e.detail.value;
+  },
+  note_content: function(e){
+    this.data.note_content = e.detail.value;
+  },
+  note_cancle: function(){
+    wx.navigateBack({
+      delta: 1
+    })
+  },
+  note_save: function() {
+    console.log(this.data)
+    wx.navigateBack({
+      delta: 1
+    })
+    wx.request({
+      url: '',
+      data: {
+      },
+      header: {
+        'content-type': 'application/json'
+      },
+      complete: res=>{
+        console.log(res.data)
+      }
+    })
   }
 })
 
--- pages/list/list.js
+++ pages/list/list.js
@@ -5,14 +5,27 @@ const app = getApp()
 Page({
   onLoad: function () {
   },
-  onclick: function(){
+  onShow() {
+    this.onLoad
+    console.log("reload")
+  },
+  add_note: function(){
     console.log('test')
     wx.navigateTo({
       url: '../add_content/add_content'
     })
+  },
+
+  add_type: function () {
+    console.log('test')
+    wx.navigateTo({
+      url: '../add_type/add_type'
+    })
+  },
+
+  log: function(){
+    wx.navigateTo({
+      url: '../logs/logs',
+    })
   }
 })
-
-function onclick() {
-  console.log('test')  
-}
