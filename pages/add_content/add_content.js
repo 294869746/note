3f61c0d4ef43d797c320888f0b9e5c963c8f538b
unknown <unknown@example.com>
2018-11-10 21:52:47 +0800

    优化逻辑

--- pages/add_content/add_content.js
+++ pages/add_content/add_content.js
@@ -5,7 +5,7 @@ const util = require('../../utils/util.js')
 Page({
   data : {
     note_title: "",
-    note_type: "",
+    note_group: "",
     note_content: "" 
   },
   onLoad: function () {
@@ -14,8 +14,8 @@ Page({
   note_title: function(e){
     this.data.note_title = e.detail.value;
   },
-  note_type: function(e){
-    this.data.note_type = e.detail.value;
+  note_group: function(e){
+    this.data.note_group = e.detail.value;
   },
   note_content: function(e){
     this.data.note_content = e.detail.value;
@@ -26,18 +26,48 @@ Page({
     })
   },
   note_save: function() {
-    var that = this 
-    console.log(this.data)
-    wx.navigateBack({
-      delta: 1
-    })
-    var note_id = util.formatTime(new Date())
-    var note_data = [that.data.note_title, that.data.note_type, that.data.note_content, note_id]
-    note_data = note_data.toString()
-    note_id = note_id.toString()
-    console.log(wx.getStorageInfoSync().currentSize)
-    console.log(wx.getStorageSync('2'))
-    wx.setStorageSync(note_id, note_data)
+    var stkeys = wx.getStorageInfoSync().keys.toString()
+    var group = new Array()
+    var group_index =0
+    stkeys = stkeys.split(',')
+    for (var key=0; key < stkeys.length; key++) {
+      var content = wx.getStorageSync(stkeys[key]).toString()
+      content = content.split(',')
+      if (content.length == 2) {
+        group.push(content[1])
+      }
+    }
+    for(var i=0;i<group.length;i++){
+      var input_group = this.data.note_group.toString()
+      var exist_group = group[i]
+      var flag = true
+      if(input_group == exist_group)
+      {
+        flag = false
+        break
+      }
+    }
+    if (flag)
+      {
+        wx.showToast({
+          title: '您的分组不存在',
+          icon: 'loading'
+        })
+        this.setData({
+          note_group: ''
+        })
+      }
+      else{
+        wx.navigateBack({
+          delta: 1
+        })
+        var note_id = new Date().getTime()
+        var note_data = [this.data.note_title, this.data.note_group, this.data.note_content, note_id]
+        note_data = note_data.toString()
+        note_id = note_id.toString()
+        wx.setStorageSync(note_id, note_data)
+      }
+
   }
 })
 
--- pages/list/list.js
+++ pages/list/list.js
@@ -3,7 +3,7 @@
 const app = getApp()
 const util = require('../../utils/util.js')
 
-Array.prototype.distinct = function () {
+Array.prototype.distinct = function() {
   var arr = this,
     result = [],
     i,
@@ -21,39 +21,54 @@ Array.prototype.distinct = function () {
 }
 
 Page({
-  data: {
-    list_data: [{
-      date: '',
-      message: '',
-      group: ''
-    }],
-    list_group: ['']
-  },
+  data: {},
   onLoad: function() {
+    // wx.clearStorageSync()
   },
   onShow() {
-    var index = 0
+    var group_index = 0
+    var note_index = 0
     var key = 0
     var stkeys = wx.getStorageInfoSync().keys.toString()
     var group = new Array()
+    var note = new Array()
     stkeys = stkeys.split(',')
     for (key; key < stkeys.length; key++) {
       var content = wx.getStorageSync(stkeys[key]).toString()
       content = content.split(',')
       if (content.length == 2) {
-        var gname = "list_group[" + index + "]"
-          group.push(content[1])
-        index = index + 1
+        var gname = "list_group[" + group_index + "]"
+        group.push(content[1])
+        group_index = group_index + 1
+      } else if (content.length == 4) {
+        var nname = "list_data[" + note_index + "]"
+        note.push(content)
+        note_index = note_index + 1
       }
     }
     group = group.distinct()
-    for(var i=0;i<group.length;i++)
-    {
-      index = "list_group["+i+"]"
+    for (var i = 0; i < group.length; i++) {
+      group_index = "list_group[" + i + "]"
       this.setData({
-        [index]: group[i] 
+        [group_index]: group[i]
       })
     }
+    for (var i = 0; i < note.length; i++) {
+      var note_title_index = "list_note[" + i + "].title"
+      var note_group_index = "list_note[" + i + "].group"
+      var note_date_index = "list_note[" + i + "].date"
+      this.setData({
+        [note_title_index]: note[i][0]
+      })
+      this.setData({
+        [note_group_index]: note[i][1]
+      })
+      this.setData({
+        [note_date_index]: note[i][3]
+      })
+    }
+    //添加笔记信息
+
   },
   add_note: function() {
     wx.navigateTo({
@@ -62,12 +77,31 @@ Page({
   },
 
   add_type: function() {
-    console.log('test')
     wx.navigateTo({
       url: '../add_type/add_type'
     })
   },
 
+  clear_data: function() {
+    var that = this
+    wx.showModal({
+      title: '提示',
+      content: '确认删除所有数据吗？',
+      success: function(res) {
+        if (res.confirm) {
+          wx.clearStorageSync()
+
+          that.setData({
+            'list_note': ''
+          })
+          that.setData({
+            'list_group': ''
+          })
+          that.onLoad()
+        }
+      }
+    })
+  },
   log: function() {
     wx.navigateTo({
       url: '../logs/logs',
